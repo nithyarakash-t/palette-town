f98967f3c421d234a599e35e4479b8889536b7fa
nithyaprakash-t <nithyaprakash.t@gmail.com>
2025-01-17 00:02:34 +0530

    Refactor CSS structure and integrate Chart component into CssColors

--- src/css-colors/Csscolors.tsx
+++ src/css-colors/Csscolors.tsx
@@ -1,12 +1,13 @@
 import { Slider } from "./slider/Slider";
 import './Csscolors.scss';
+import { Chart } from "./chart/Chart";
 
 export function CssColors() {
     return (
         <div className="app-grid cc-main__wrap">
             <div className="cc-main__inner">
                 <Slider />
-                <CssColors/>
+                <Chart/>
             </div>
         </div>
     )
--- src/css-colors/chart/Chart.tsx
+++ src/css-colors/chart/Chart.tsx
@@ -2,6 +2,12 @@ import './Chart.scss';
 
 export function Chart() {
     return (
-        <></>
+        <div className='cc-chart__wrap'>
+            <div className='cc-chart__container'>
+                <div className='cc-chart__row'>
+                    
+                </div>
+            </div>
+        </div>
     )
 }
