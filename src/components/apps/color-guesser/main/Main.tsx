c845b964dcb8eb7d1ade9ae88747fc80df50165c
nithyaprakash-t <nithyaprakash.t@gmail.com>
2025-01-23 00:45:11 +0530

    completed css colours

--- src/components/apps/color-guesser/main/Main.tsx
+++ src/components/apps/color-guesser/main/Main.tsx
@@ -1,12 +1,14 @@
 import './Main.scss';
 import { data, UniqueColorItem } from '../data/data';
 import { FormEvent, useEffect, useState } from 'react';
+import { ColorHEX, ColorRGB, colorSimilarity, getRandomIntInclusive, hexToRGB } from '../utils';
 
 export function Main() {
-    const [color, setColor] = useState<UniqueColorItem | null>(null);
+    const [color, setColor] = useState<UniqueColorItem>(getRandomColor());
+    const [inputColor, setInputColor] = useState<ColorHEX>(getRandomColor().hex as ColorHEX);
+    const [similarity, setSimilarity] = useState<number | null>(null);
 
     useEffect(()=>{
-        setColor(getRandomColor());        
     },[])
 
     function getRandomColor() {
@@ -15,39 +17,39 @@ export function Main() {
 
     function handleColorInput(e:FormEvent) {
         const INPUT_COLOR = (e.currentTarget as HTMLInputElement).value;
-        compareColors(INPUT_COLOR.toLowerCase(), color?.hex.toLowerCase() ?? '#000000')
+        setInputColor(INPUT_COLOR as ColorHEX);
+                
     }
 
-    function compareColors(color1:string, color2:string) {
-        console.log(color1, color2);
+    function handleSubmit() {
+        const deltaE = colorSimilarity(hexToRGB(inputColor), color.rgb as ColorRGB);
+        setSimilarity(parseFloat((deltaE * 100).toFixed(2)));
+    }
+
+    function handleReset() {
+        setSimilarity(null);
+        setColor(getRandomColor());
+        setInputColor(getRandomColor().hex as ColorHEX)
     }
 
     return (
         <section className='cg-main__wrap'>
             <label className='cg-main__input'>
-                <input type='color' aria-label='Choose color' name='cg-input' id='cg-input' onInput={(e)=>handleColorInput(e)}/>
+                <input type='color' aria-label='Choose color' name='cg-input' id='cg-input'
+                value={inputColor} onInput={(e)=>handleColorInput(e)}/>
             </label>
             <p className='cg-main__color'>{color ? `${color.name}` : 'Choose new color'}</p>
 
             <div className='cg-main__btngroup'>
-                <button type='button' className='cg-main__submit' aria-label='Submit your guess'>
+                <button type='button' className='cg-main__submit' aria-label='Submit your guess' onClick={handleSubmit}>
                     Submit
                 </button>
                 <button type='button' className='cg-main__reset'
-                onClick={()=>{return setColor(getRandomColor());}} aria-label='Reset to a different color'>Reset</button>
+                onClick={handleReset} aria-label='Reset to a different color'>Reset</button>
             </div>
+
+            {similarity && <p  className='cg-main__result'>{similarity}%</p>}
         </section>
     )
 }
 
-function getRandomIntInclusive(min:number, max:number) {
-    const RANDOM_BUFFER = new Uint32Array(1);
-
-    window.crypto.getRandomValues(RANDOM_BUFFER);
-
-    const RANDOM_NUMBER = RANDOM_BUFFER[0] / (0xffffffff + 1);
-
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(RANDOM_NUMBER * (max - min + 1)) + min;
-}
--- src/components/layout/releasenotes/release-notes.ts
+++ src/components/layout/releasenotes/release-notes.ts
@@ -1,5 +1,12 @@
 export const releaseNotes = [
     {
+        version: "2.0.0",
+        date: "23.01.2025",
+        desc: [
+            "Completed color guesser -- UI pending",
+        ]
+    },
+    {
         version: "1.0.0",
         date: "16.01.2025",
         desc: [
