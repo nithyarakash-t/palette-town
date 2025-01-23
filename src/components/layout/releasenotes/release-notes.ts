b2f9ee211eace01b9dee448f7d0cfe83ecb15e6a
nithyaprakash-t <nithyaprakash.t@gmail.com>
2025-01-24 00:56:45 +0530

    updated release-notes

--- src/components/layout/releasenotes/release-notes.ts
+++ src/components/layout/releasenotes/release-notes.ts
@@ -1,9 +1,10 @@
 export const releaseNotes = [
     {
-        version: "2.0.0",
+        version: "2.1.0",
         date: "23.01.2025",
         desc: [
-            "Completed color guesser -- UI pending",
+            "2.0.0 - Completed color guesser -- UI pending",
+            "2.1.0 - 24.01.2025 - Updated color guesser with difficulty modes",
         ]
     },
     {
