b31feb881f02b1541a65a54195d3d8ec4c092ede
nithyaprakash-t <nithyaprakash.t@gmail.com>
2025-02-10 23:48:06 +0530

    Add theme switch component and update styles; enhance meta tags in index.html

--- src/components/layout/themeSwitch/Themeswitch.tsx
+++ src/components/layout/themeSwitch/Themeswitch.tsx
@@ -39,12 +39,12 @@ export function Themeswitch() {
             data-theme={theme}
             onClick={()=>handleClick(theme === 'dark' ? 'light' : 'dark')}>
                 <svg className="sun-and-moon" aria-hidden="true" width="24" height="24" viewBox="0 0 24 24">
-                    <mask className="moon" id="moon-mask">
+                  <mask className="moon" id="moon-mask">
                     <rect x="0" y="0" width="100%" height="100%" fill="white" />
                     <circle cx="24" cy="10" r="6" fill="black" />
-                    </mask>
-                    <circle className="sun" cx="12" cy="12" r="6" mask="url(#moon-mask)" fill="currentColor" />
-                    <g className="sun-beams" stroke="currentColor">
+                  </mask>
+                  <circle className="sun" cx="12" cy="12" r="6" mask="url(#moon-mask)" fill="currentColor" />
+                  <g className="sun-beams" stroke="currentColor">
                     <line x1="12" y1="1" x2="12" y2="3" />
                     <line x1="12" y1="21" x2="12" y2="23" />
                     <line x1="4.22" y1="4.22" x2="5.64" y2="5.64" />
@@ -53,7 +53,7 @@ export function Themeswitch() {
                     <line x1="21" y1="12" x2="23" y2="12" />
                     <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" />
                     <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" />
-                    </g>
+                  </g>
                 </svg>
             </button>
         </div>
