fe541cf4d14aba0bde54873b474f025c8698e98e
nithyaprakash-t <nithyaprakash.t@gmail.com>
2025-02-24 00:05:47 +0530

    Update project version to 0.3.0; revise release notes and adjust previous version numbers

--- src/components/layout/releasenotes/release-notes.ts
+++ src/components/layout/releasenotes/release-notes.ts
@@ -1,15 +1,22 @@
 export const releaseNotes = [
     {
-        version: "2.1.1",
-        date: "23.01.2025",
+        version: "0.3.0",
+        date: "22.02.2025",
         desc: [
-            "2.0.0 - Completed color guesser -- UI pending",
-            "2.1.0 - 24.01.2025 - Updated color guesser with difficulty modes",
-            "2.1.1 - 25.01.2025 - Updated navmenu logic to track page changes, fixed controlled input error in color guesser"
+            "Completed contrast checker",
         ]
     },
     {
-        version: "1.0.0",
+        version: "0.2.2",
+        date: "23.01.2025",
+        desc: [
+            "0.2.0 - Completed color guesser -- UI pending",
+            "0.2.1 - 24.01.2025 - Updated color guesser with difficulty modes",
+            "0.2.2 - 25.01.2025 - Updated navmenu logic to track page changes, fixed controlled input error in color guesser"
+        ]
+    },
+    {
+        version: "0.1.0",
         date: "16.01.2025",
         desc: [
             "Deployed CSS colors",
