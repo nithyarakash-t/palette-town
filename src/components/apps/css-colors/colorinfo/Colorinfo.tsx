d8ce250cffec13efbee2626ceaf82d0290dfcd5b
Nithyaprakash T <nithyaprakash.t@gmail.com>
2026-05-23 14:50:38 +0530

    added copy to csscolors

--- src/components/apps/css-colors/colorinfo/Colorinfo.tsx
+++ src/components/apps/css-colors/colorinfo/Colorinfo.tsx
@@ -9,11 +9,28 @@ export function ColorInfo({selectedColor, setSelectedColor}
             <div className={`cc-colorinfo__container ${selectedColor.type === 'dark' ? '-dark' : '-light'}`}>
                 <h2>
                     <span>{selectedColor.name}</span>
+                    <CopyToClipboard textToCopy={selectedColor.name}/>
                 </h2>
-                <p>{selectedColor.hex}</p>
-                <p>{formatHSL(selectedColor.hsl)}</p>
-                <p>{formatRGB(selectedColor.rgb)}</p>
-                {selectedColor.alternativeName && <p>Alt name: {selectedColor.alternativeName}</p>}
+                <p>
+                    <span>{selectedColor.hex}</span>
+                    <CopyToClipboard textToCopy={selectedColor.hex}/>
+                </p>
+                <p>
+                    <span>{formatHSL(selectedColor.hsl)}</span>
+                    <CopyToClipboard textToCopy={formatHSL(selectedColor.hsl)}/>
+                </p>
+                <p>
+                    <span>{formatRGB(selectedColor.rgb)}</span>
+                    <CopyToClipboard textToCopy={formatRGB(selectedColor.rgb)}/>
+                </p>
+                {
+                    selectedColor.alternativeName 
+                    && 
+                    <p>
+                        <span>Alt name: {selectedColor.alternativeName}</span>
+                        <CopyToClipboard textToCopy={selectedColor.alternativeName}/>
+                    </p>
+                }
 
                 <button type='button' aria-label='Back' onClick={()=>setSelectedColor(null)}>
                     <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 1024 1024"><path fill="currentColor" d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z"/><path fill="currentColor" d="m237.248 512l265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z"/></svg>
@@ -22,4 +39,11 @@ export function ColorInfo({selectedColor, setSelectedColor}
             </div>
         </div>
     )
+}
+
+function CopyToClipboard({textToCopy}:{readonly textToCopy: string}) {
+    const handleCopy = () => {
+        navigator.clipboard.writeText(textToCopy);
+    }
+    return <button type='button' className='cc-colorinfo__copy' aria-label='copy to clipboard' onClick={handleCopy}></button>
 }
